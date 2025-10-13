dc4eec806dc4ac8a48365985a2662e4f6cab2e72
ferna <ferna@Fernando>
2025-10-12 22:06:31 -0600

    Inicialización del proyecto Flowa

---
+++ src/main/java/com/mycompany/flowa/Flowa.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.flowa;
+
+/**
+ *
+ * @author ferna
+ */
+public class Flowa {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
